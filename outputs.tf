6e4127cd5642079e352c6c17a70d6011cea120c9
kiranug <kiran.ug123@gmail.com>
2025-04-15 19:20:24 +0530

    added module for rg

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,10 @@
 output "resource_group_name" {
-  description = "The name of the created resource group"
-  value       = module.resource_group.name
+  value = module.resource_group.name
 }
 
 output "vnet_name" {
   description = "The name of the created virtual network"
-  value       = azurerm_virtual_network.main.name
+  value       = module.vnet.name
 }
 
 # Add more outputs as needed
