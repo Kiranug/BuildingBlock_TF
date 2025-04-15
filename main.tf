f7886b10fc19e3a1b34802aafd305ce699d41ac5
Kiranug <kiran.ug123@gmail.com>
2025-04-15 18:55:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,32 +4,53 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
   }
 
-  backend "azurerm" {
-    # Will be configured during init
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
 }
 
-module "resource_group" {
-  source = "./modules/resource_group"
+provider "random" {}
 
-  name     = var.resource_group_name
+# Generate a random suffix for naming
+resource "random_id" "common" {
+  byte_length = 2
+}
+
+locals {
+  suffix = random_id.common.hex
+
+  resource_group_name = "${var.environment}-${var.location}-rg-${local.suffix}"
+  vnet_name           = "${var.environment}-${var.location}-vnet-${local.suffix}"
+  storage_name        = lower("st${local.suffix}")
+}
+
+# Conditionally create resource group
+module "resource_group" {
+  source   = "./modules/resource_group"
+  name     = local.resource_group_name
   location = var.location
   tags = {
     environment = var.environment
   }
+
+  count = var.deploy_rg ? 1 : 0
 }
 
-# Add your resources here
-# Example: Azure Virtual Network
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.environment}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
+# Conditionally create VNet
+module "vnet" {
+  source              = "./modules/vnet"
+  name                = local.vnet_name
+  location            = var.location
+  resource_group_name = module.resource_group[0].name
+  depends_on          = [module.resource_group]
+
+  count = var.deploy_vnet ? 1 : 0
 }
