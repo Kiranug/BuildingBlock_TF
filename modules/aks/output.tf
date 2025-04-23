d447b6737b7ccf22bfb939e813029c48ff94fb7b
kiranug <kiran.ug123@gmail.com>
2025-04-23 17:49:36 +0530

    output "kubelet_identity_id" {   value = azurerm_kubernetes_cluster.aks.identity[0].principal_id }

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -2,3 +2,7 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+output "kubelet_identity_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
