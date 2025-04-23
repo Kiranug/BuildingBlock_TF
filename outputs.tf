b92a5daabaee40073692166532e524cb85ec1eb8
kiranug <kiran.ug123@gmail.com>
2025-04-23 17:01:24 +0530

    commented output

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
-output "vnet_name" {
-  value = module.vnet.vnet_name
-}
+# output "vnet_name" {
+#   value = module.vnet.vnet_name
+# }
 
-output "vnet_id" {
-  value = module.vnet.vnet_id
-}
+# output "vnet_id" {
+#   value = module.vnet.vnet_id
+# }
 
-output "subnet_names" {
-  value = module.vnet.subnet_names
-}
+# output "subnet_names" {
+#   value = module.vnet.subnet_names
+# }
 
-output "subnet_ids" {
-  value = module.vnet.subnet_ids
-}
+# output "subnet_ids" {
+#   value = module.vnet.subnet_ids
+# }
 
 output "resource_group_name" {
   value = module.resource_group.name  
